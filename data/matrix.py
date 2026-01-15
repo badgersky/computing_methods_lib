d82fa4f1b5895d0bf6a6a3ffbd63251c74a70186
szymon <szymon.pt4sznik@gmail.com>
2026-01-16 00:09:18 +0100

    added validation of arguments in constructor and setter in vector and matrix classes

--- data/matrix.py
+++ data/matrix.py
@@ -3,7 +3,7 @@ from typing import List
 class Matrix():
 
     def __init__(self, data: List[List[float]]):
-        self._m = [[float(el) for el in row] for row in data]
+        self._m = self._check_data(data)
 
     def size(self) -> tuple[int]:
         return (len(self._m), len(self._m[0]))
@@ -14,7 +14,7 @@ class Matrix():
     
     @m.setter
     def m(self, value: List[List[float]]):
-        self._m = value
+        self._m = self._check_data(value)
 
     def __str__(self):
         res: str = ''
@@ -22,16 +22,33 @@ class Matrix():
             res += ' '.join(str(el) for el in row) + '\n'
 
         return res[:-1]
+    
+    @staticmethod
+    def _check_data(data: List[List[float]]) -> List[List[float]]:
+        if len(data) == 0:
+            raise ValueError('Matrix must have at least one row')
         
+        r_len = len(data[0])
+        if r_len == 0:
+            raise ValueError('Matrix must have at least one column')
+        
+        for row in data:
+            if len(row) != r_len:
+                raise ValueError('Matrix rows have different sizes')
+        
+        try:
+            res = [[float(el) for el in row] for row in data]
+        except (ValueError, TypeError):
+            raise ValueError('Matrix elements must be numbers')
+        
+        return res
+
     def get_item(self, i: int, j: int) -> float:
-        if isinstance(i, int) and isinstance(j, int):
-            return self._m[i][j]
+        return self._m[i][j]
         
     def set_item(self, i: int, j: int, num: float) -> None:
         num: float = float(num)
-        if isinstance(i, int) and isinstance(j, int) and isinstance(num, float):
-            self._m[i][j] = num
+        self._m[i][j] = num
     
     def get_row(self, i: int) -> List[float]:
-        if isinstance(i, int):
-            return self._m[i]
+        return self._m[i]
--- data/vector.py
+++ data/vector.py
@@ -2,7 +2,7 @@ from typing import List
 
 class Vector:
     def __init__(self, data: List[float]):
-        self._v = [float(num) for num in data]
+        self._v = self._check_data(data)
 
     def size(self) -> int:
         return len(self._v)
@@ -13,19 +13,28 @@ class Vector:
     
     @v.setter
     def v(self, value: List[float]):
-        self._v = value
+        self._v = self._check_data(value)
 
     def __str__(self):
         return f'{' '.join([str(self._v[i]) for i in range(len(self._v))])}'
 
+    @staticmethod
+    def _check_data(data: List[float]) -> List[float]:
+        if len(data) == 0:
+            raise ValueError('Vector must have at least one element')
+        try:
+            res = [float(el) for el in data]
+        except (TypeError, ValueError):
+            raise ValueError('Vector elements must be numbers')
+
+        return res
+
     def get_item(self, i: int) -> float:
-        if isinstance(i, int):
-            return self._v[i]
+        return self._v[i]
         
     def set_item(self, i: int, num: float):
         num = float(num)
-        if isinstance(i, int) and isinstance(num , float):
-            self._v[i] = num
+        self._v[i] = num
 
     def max_norm(self) -> float:
         return max([abs(el) for el in self._v])
