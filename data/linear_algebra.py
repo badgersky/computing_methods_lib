30781feb360c09e5cb79037e7bb4d78e1747918f
szymon <szymon.pt4sznik@gmail.com>
2026-01-15 05:08:54 +0100

    added set item method to Matrix class, added mathod for multiplying matrixes in linear algebra class

--- data/linear_algebra.py
+++ data/linear_algebra.py
@@ -3,6 +3,7 @@ from matrix import Matrix
 
 class LinearAlgebra():
 
+    @staticmethod
     def sum_vectors(v1: Vector, v2: Vector) -> Vector:
         s1, s2 = v1.size(), v2.size()
         if s1 != s2:
@@ -10,6 +11,7 @@ class LinearAlgebra():
 
         return Vector([v1[i] + v2[i] for i in range(s1)])
     
+    @staticmethod
     def sub_vectors(v1: Vector, v2: Vector) -> Vector:
         s1, s2 = v1.size(), v2.size()
         if s1 != s2:
@@ -17,7 +19,28 @@ class LinearAlgebra():
 
         return Vector([v1[i] - v2[i] for i in range(s1)])
 
+    @staticmethod
     def sub_matrix(m1: Matrix, m2: Matrix) -> Matrix:
         s1, s2 = m1.size(), m2.size()
         if s1 != s2:
             raise ValueError('Matrixes have different sizes')
+        
+    @staticmethod
+    def mult_mm(m1: Matrix, m2: Matrix) -> Matrix:
+        rm1, cm1 = m1.size()
+        rm2, cm2 = m2.size()
+
+        if cm1 != rm2:
+            raise ValueError('Matrixes cannot be multiplied')
+        
+        res = Matrix([[0. for _ in range(cm2)] for _ in range(rm1)])
+
+        for i in range(rm1):
+            for j in range(cm2):
+                for k in range(cm1):
+                    item = res.get_item(i, j)
+                    item += m1.get_item(i, k) * m2.get_item(k, j)
+                    res.set_item(i, j, item)
+
+        return res
+    
--- data/matrix.py
+++ data/matrix.py
@@ -12,6 +12,11 @@ class Matrix():
         if isinstance(i, int) and isinstance(j, int):
             return self._m[i][j]
         
+    def set_item(self, i: int, j: int, num: float) -> None:
+        num = float(num)
+        if isinstance(i, int) and isinstance(j, int) and isinstance(num, float):
+            self._m[i][j] = num
+    
     def get_row(self, i: int) -> List[float]:
         if isinstance(i, int):
             return self._m[i]
